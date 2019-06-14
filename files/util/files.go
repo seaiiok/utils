feedfb462651649b9f79be7afee7921cd13c84cf
seaiiok <seaii@qq.com>
2019-06-14 15:09:17 +0800

    update...

--- files/util/files.go
+++ files/util/files.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"unicode"
 )
@@ -55,8 +56,23 @@ func (fi *files) GetFilterSuffix(suffix ...string) *files {
 	return fi
 }
 
+func (fi *files) GetFilterReg(r string) *files {
+	i := 0
+
+	for k, v := range fi.FilesList {
+		reg := regexp.MustCompile("KNR")
+		if reg.MatchString(v) {
+			fi.FilesList[i] = fi.FilesList[k]
+			i++
+			continue
+		}
+	}
+	fi.FilesList = fi.FilesList[:i]
+	return fi
+}
+
 func (fi *files) GetFilterZH() *files {
-	i:=0
+	i := 0
 	for k, v := range fi.FilesList {
 		if !isChineseChar(v) {
 			fi.FilesList[i] = fi.FilesList[k]
--- files/util/files_test.go
+++ files/util/files_test.go
@@ -5,19 +5,19 @@ import "testing"
 func TestGetAllFiles(t *testing.T) {
 f:=new(File)
 
-f2:=f.GetAllFiles("D:\\FTP\\online\\ABC")
-for _, v := range f2.FilesList {
+f1:=f.GetAllFiles("D:\\FTP\\online\\ABC").GetFilterSuffix(".zip").GetFilterZH()
+for _, v := range f1.FilesList {
 	t.Log(v)
 }
-t.Log("-------------------------")
-f2=f2.GetFilterSuffix(".zip")
-for _, v := range f2.FilesList {
-	t.Log(v)
-}
-t.Log("-------------------------")
-f2=f2.GetFilterZH()
-for _, v := range f2.FilesList {
-	t.Log(v)
-}
+// t.Log("-------------------------")
+// f1=f1.GetFilterSuffix(".zip")
+// for _, v := range f.FilesList {
+// 	t.Log(v)
+// }
+// t.Log("-------------------------")
+// f1=f1.GetFilterZH()
+// for _, v := range f.FilesList {
+// 	t.Log(v)
+// }
 
 }
