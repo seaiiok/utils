060b76d977eed2dcefb00d1b92e69cc1875c0a01
seaiiok <seaii@qq.com>
2019-06-22 11:46:16 +0800

    update...

--- fmtx/fmtx.go
+++ fmtx/fmtx.go
@@ -2,6 +2,7 @@ package fmtx
 
 import (
 	"fmt"
+	"runtime"
 	"syscall"
 )
 
@@ -17,23 +18,41 @@ const (
 )
 
 func Println(i int, a ...interface{}) (int, error) {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	proc := kernel32.NewProc("SetConsoleTextAttribute")
-	handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
-	n, err := fmt.Println(a...)
-	handle, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
-	CloseHandle := kernel32.NewProc("CloseHandle")
-	CloseHandle.Call(handle)
-	return n, err
+	if IsWindows() {
+		kernel32 := syscall.NewLazyDLL("kernel32.dll")
+		proc := kernel32.NewProc("SetConsoleTextAttribute")
+		handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
+		n, err := fmt.Println(a...)
+		handle, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
+		CloseHandle := kernel32.NewProc("CloseHandle")
+		CloseHandle.Call(handle)
+		return n, err
+	} else {
+		return fmt.Println(a...)
+	}
+
 }
 
 func Printf(i int, format string, a ...interface{}) (int, error) {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	proc := kernel32.NewProc("SetConsoleTextAttribute")
-	handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
-	n, err := fmt.Println(fmt.Sprintf(format, a...))
-	handle, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
-	CloseHandle := kernel32.NewProc("CloseHandle")
-	CloseHandle.Call(handle)
-	return n, err
+	if IsWindows() {
+		kernel32 := syscall.NewLazyDLL("kernel32.dll")
+		proc := kernel32.NewProc("SetConsoleTextAttribute")
+		handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
+		n, err := fmt.Println(fmt.Sprintf(format, a...))
+		handle, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
+		CloseHandle := kernel32.NewProc("CloseHandle")
+		CloseHandle.Call(handle)
+		return n, err
+	} else {
+		return fmt.Println(fmt.Sprintf(format, a...))
+	}
+
+}
+
+func IsWindows() bool {
+	if runtime.GOOS == "windows" {
+		return true
+	} else {
+		return false
+	}
 }
